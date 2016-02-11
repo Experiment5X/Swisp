6c3304f592da13a66df824c1ff265aaf0a9e2a62
Adam Spindler <xmeadamx@gmail.com>
2016-02-11 16:35:24 -0500

    Initial commit

---
+++ Swisp/Interpreter/Interpreter.swift
@@ -0,0 +1,9 @@
+//
+//  Interpreter.swift
+//  Swisp
+//
+//  Created by Adam Spindler on 2/11/16.
+//  Copyright © 2016 Adam Spindler. All rights reserved.
+//
+
+import Foundation
---
+++ Swisp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swisp
+//
+//  Created by Adam Spindler on 2/11/16.
+//  Copyright © 2016 Adam Spindler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
